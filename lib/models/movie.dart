2ed6aa6aecd2b2989252c003b89d86fac5051b9b
Doublepor <doublepor.pp@gmail.com>
2020-12-22 18:38:18 +0700

    Remove Favorite

--- lib/models/movie.dart
+++ lib/models/movie.dart
@@ -2,7 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Movie {
   final String wallpaper, posterURL, review, title, year;
-  final List<dynamic> category;
+  final List<dynamic> category, uid;
   final int rating;
 
   final DocumentReference reference;
@@ -14,7 +14,8 @@ class Movie {
         title = map["title"],
         rating = map["rating"],
         year = map["year"],
-        category = map["category"];
+        category = map["category"],
+        uid = map["uid"];
 
   Movie.fromSnapshot(DocumentSnapshot snapshot)
       : this.fromMap(snapshot.data(), reference: snapshot.reference);
--- lib/views/detail/components/btnfavourtie.dart
+++ lib/views/detail/components/btnfavourtie.dart
@@ -20,37 +20,59 @@ class BuildFavourBtn extends StatelessWidget {
     return Container(
       width: MediaQuery.of(context).size.width * 0.95,
       height: MediaQuery.of(context).size.height * 0.06,
-      child: RaisedButton(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10),
-        ),
-        color: Colors.red[900],
-        child: Text(
-          "Add Favourite",
-          style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-            fontSize: 20,
-          ),
-        ),
-        onPressed: () {
-          FirebaseFirestore.instance
-              .collection("movies")
-              .doc(documentsID)
-              .update({
-            "uid": FieldValue.arrayUnion([firebaseUser.uid])
-          });
-        },
-      ),
+      child: (record.uid.indexOf(firebaseUser.uid) == -1)
+          ? buildRaisedButtonAdd()
+          : buildRaisedButtonRemove(),
     );
   }
-}
 
-class TestEiei extends StatelessWidget {
-  const TestEiei({Key key}) : super(key: key);
+  RaisedButton buildRaisedButtonAdd() {
+    return RaisedButton(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      color: Colors.red[900],
+      child: Text(
+        "Add Favourite",
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+          fontSize: 20,
+        ),
+      ),
+      onPressed: () {
+        FirebaseFirestore.instance
+            .collection("movies")
+            .doc(documentsID)
+            .update({
+          "uid": FieldValue.arrayUnion([firebaseUser.uid])
+        });
+      },
+    );
+  }
 
-  @override
-  Widget build(BuildContext context) {
-    return Container();
+  RaisedButton buildRaisedButtonRemove() {
+    return RaisedButton(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      color: Colors.red[900],
+      child: Text(
+        "Remove Favourite",
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+          fontSize: 20,
+        ),
+      ),
+      onPressed: () {
+        FirebaseFirestore.instance
+            .collection("movies")
+            .doc(documentsID)
+            .update({
+          "uid": FieldValue.arrayRemove([firebaseUser.uid])
+        });
+      },
+    );
   }
 }
